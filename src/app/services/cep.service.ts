ec0ec5f5f6a6634d10ad8027c14bb30b9370ed75
Luís <152504988+lu0019@users.noreply.github.com>
2024-05-23 23:26:41 -0300

    Reestruturei o comando da conexão com a API Comecei a estrutura pro botão de consulta

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,18 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CEPService {
-  public cep:string = "";
-  public caminhoBase:string = "https://viacep.com.br/ws/";
-  public formatoRetorno:string = "/json/";
 
   constructor(public http: HttpClient) { }
   
-  public consultarCEP() {
-    let endpoint:string = `${this.caminhoBase}${this.cep}${this.formatoRetorno}`;
-    return this.http.get(endpoint);
+  consultarCEP(cep: string): Observable<any> {
+    return this.http.get(`https://viacep.com.br/ws/${cep}/json/`);
   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -9,7 +9,36 @@ import { CEPService } from '../services/cep.service';
 })
 export class Tab2Page {
 
+  public cep:string = "";
+  public logradouro:string = "";
+  public complemento:string = "";
+  public bairro:string = "";
+  public localidade:string = "";
+  public uf:string = "";
+  public ibge:string = "";
+  public gia:string = "";
+  public ddd:string = "";
+  public siafi:string = "";
+
   constructor(public cepService: CEPService) {}
 
-  
+  botaoConsulta() {
+    if (this.cep) {
+      this.cepService.consultarCEP(this.cep).subscribe((data) => {
+        if (!data.erro) {
+          this.logradouro = data.logradouro;
+          this.complemento = data.complemento;
+          this.bairro = data.bairro;
+          this.localidade = data.localidade;
+          this.uf = data.uf;
+          this.ibge = data.ibge;
+          this.gia = data.gia;
+          this.ddd = data.ddd;
+          this.siafi = data.siafi;
+        } else {
+          console.error("Desculpe, mas o CEP não foi encontrado.");
+        }
+      })
+    }
+  }
 }
