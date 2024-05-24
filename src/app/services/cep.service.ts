07c752d82f96ad0852aa8c242f1d7243650f2871
Luís <152504988+lu0019@users.noreply.github.com>
2024-05-23 21:57:46 -0300

    Montei a estrutura para conectar com a API Conectei o arquivo TS do serviço com o arquivo TS da Tab 2 Fiz com que o campo input capture o valor inserido

--- src/app/services/cep.service.ts
+++ src/app/services/cep.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CEPService {
+  public cep:string = "";
+  public caminhoBase:string = "https://viacep.com.br/ws/";
+  public formatoRetorno:string = "/json/";
 
-  constructor() { }
+  constructor(public http: HttpClient) { }
+  
+  public consultarCEP() {
+    let endpoint:string = `${this.caminhoBase}${this.cep}${this.formatoRetorno}`;
+    return this.http.get(endpoint);
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CEPService } from '../services/cep.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
+  providers: [CEPService]
 })
 export class Tab2Page {
 
