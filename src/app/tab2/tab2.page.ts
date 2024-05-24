36380390bb8f2a04c38718d8fbec25eca584d7f3
Luís <152504988+lu0019@users.noreply.github.com>
2024-05-23 21:44:09 -0300

    Deletei o serviço para criar outro novo

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { ServicesService } from '../CEP/cep.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  providers: [ServicesService]
 })
 export class Tab2Page {
 
