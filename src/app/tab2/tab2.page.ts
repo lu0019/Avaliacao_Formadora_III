c2aea8ee5ae47adf7e9839f69d5b92c3ba504816
Luís <152504988+lu0019@users.noreply.github.com>
2024-05-23 21:39:09 -0300

    Criei a estrutura para acessar a API Inicei a programação na tab 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ServicesService } from '../CEP/cep.service';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
+  providers: [ServicesService]
 })
 export class Tab2Page {
 
